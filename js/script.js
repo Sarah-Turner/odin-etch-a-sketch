2329c7894c33a233e16bef4ad44cbd3411ba6d87
Sarah-Turner <81490363+Sarah-Turner@users.noreply.github.com>
2026-02-23 15:47:44 -0800

    Fix createGrid to generate square length
    The function was not getting the length of the container,
    so the calculation for the length of the squares was incorrect.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 function createGrid(gridSize) {
+    if (gridSize < 1) {
+        gridSize = 1;
+    } else if (gridSize > 100) {
+        gridSize = 100;
+    }
     for (let i = 0; i < gridSize * gridSize; i++) {
         let square = document.createElement("div");
         square.classList.toggle("square");
         let container = document.querySelector("#grid-container");
-        let containerLength = container.style.width;
+        let containerLength = container.offsetWidth;
         let squareSize = containerLength / gridSize;
-        square.style.width = `${squareSize} px`;
-        square.style.height = `${squareSize} px`;
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
         container.appendChild(square);
     }
 }
 
-createGrid(16);
+createGrid(1);
